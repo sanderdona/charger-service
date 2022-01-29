636e973cd06653be8aec7d935211a8de41d80dcf
sanderdona <spdona@gmail.com>
2022-01-29 17:26:31 +0100

    added unit tests

--- src/main/java/nl/dimensiontech/domotics/chargerservice/controller/ChargeSessionResource.java
+++ src/main/java/nl/dimensiontech/domotics/chargerservice/controller/ChargeSessionResource.java
@@ -1,7 +1,6 @@
 package nl.dimensiontech.domotics.chargerservice.controller;
 
 import lombok.RequiredArgsConstructor;
-import nl.dimensiontech.domotics.chargerservice.domain.ChargeSession;
 import nl.dimensiontech.domotics.chargerservice.dto.ChargeSessionDto;
 import nl.dimensiontech.domotics.chargerservice.mapper.ChargeSessionMapper;
 import nl.dimensiontech.domotics.chargerservice.service.ChargeSessionService;
