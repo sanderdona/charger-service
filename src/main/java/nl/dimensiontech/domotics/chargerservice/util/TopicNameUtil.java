90230c3432e057aef8a9473b7134c5dedcf1c659
sanderdona <spdona@gmail.com>
2022-04-08 16:19:00 +0200

    Added integration tests

--- src/main/java/nl/dimensiontech/domotics/chargerservice/util/TopicNameUtil.java
+++ src/main/java/nl/dimensiontech/domotics/chargerservice/util/TopicNameUtil.java
@@ -8,18 +8,20 @@ import static nl.dimensiontech.domotics.chargerservice.constants.MqttConstants.T
 public class TopicNameUtil {
 
     public static Long getCarId(Message<?> message) {
-        String topic = (String) message.getHeaders().get(TOPIC_HEADER);
-        assert topic != null;
-
+        String topic = getTopic(message);
         String[] split = topic.split(TOPIC_SEPARATOR);
         return Long.valueOf(split[2]);
     }
 
     public static String getLastTopicName(Message<?> message) {
+        String topic = getTopic(message);
+        return topic.substring(topic.lastIndexOf("/") + 1);
+    }
+
+    private static String getTopic(Message<?> message) {
         String topic = (String) message.getHeaders().get(TOPIC_HEADER);
         assert topic != null;
-
-        return topic.substring(topic.lastIndexOf("/") + 1);
+        return topic;
     }
 
 }
