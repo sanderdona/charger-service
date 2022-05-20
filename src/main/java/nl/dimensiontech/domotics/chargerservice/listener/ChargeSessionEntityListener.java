70a320993af9ba4086679d929200139ac349f87a
sanderdona <spdona@gmail.com>
2022-05-20 08:57:55 +0200

    Use DTO for messaging

--- src/main/java/nl/dimensiontech/domotics/chargerservice/listener/ChargeSessionEntityListener.java
+++ src/main/java/nl/dimensiontech/domotics/chargerservice/listener/ChargeSessionEntityListener.java
@@ -2,6 +2,8 @@ package nl.dimensiontech.domotics.chargerservice.listener;
 
 import lombok.RequiredArgsConstructor;
 import nl.dimensiontech.domotics.chargerservice.domain.ChargeSession;
+import nl.dimensiontech.domotics.chargerservice.dto.ChargeSessionDto;
+import nl.dimensiontech.domotics.chargerservice.mapper.ChargeSessionMapper;
 import nl.dimensiontech.domotics.chargerservice.message.service.ChargeSessionMessageService;
 
 import javax.persistence.PostPersist;
@@ -11,11 +13,13 @@ import javax.persistence.PostUpdate;
 public class ChargeSessionEntityListener {
 
     private final ChargeSessionMessageService chargeSessionMessageService;
+    private final ChargeSessionMapper chargeSessionMapper;
 
     @PostPersist
     @PostUpdate
     private void afterPersistAndUpdate(ChargeSession chargeSession) {
-        chargeSessionMessageService.sendMessage(chargeSession);
+        ChargeSessionDto chargeSessionDto = chargeSessionMapper.toDto(chargeSession);
+        chargeSessionMessageService.sendMessage(chargeSessionDto);
     }
 
 }
--- src/main/java/nl/dimensiontech/domotics/chargerservice/message/service/ChargeSessionMessageService.java
+++ src/main/java/nl/dimensiontech/domotics/chargerservice/message/service/ChargeSessionMessageService.java
@@ -1,13 +1,13 @@
 package nl.dimensiontech.domotics.chargerservice.message.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import nl.dimensiontech.domotics.chargerservice.domain.ChargeSession;
+import nl.dimensiontech.domotics.chargerservice.dto.ChargeSessionDto;
 import nl.dimensiontech.domotics.chargerservice.message.handler.OutboundMessageHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ChargeSessionMessageService extends AbstractMessageService<ChargeSession> {
+public class ChargeSessionMessageService extends AbstractMessageService<ChargeSessionDto> {
 
     @Autowired
     public ChargeSessionMessageService(OutboundMessageHandler messageHandler, ObjectMapper objectMapper) {
@@ -15,8 +15,8 @@ public class ChargeSessionMessageService extends AbstractMessageService<ChargeSe
     }
 
     @Override
-    public void sendMessage(ChargeSession chargeSession) {
-        String payload = toJson(chargeSession);
+    public void sendMessage(ChargeSessionDto chargeSessionDto) {
+        String payload = toJson(chargeSessionDto);
         messageHandler.sendMessage(payload);
     }
 }
