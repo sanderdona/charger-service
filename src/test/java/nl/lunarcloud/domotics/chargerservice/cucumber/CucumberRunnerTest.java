46cbc9f3503ef81d82cb545f40d556f56f547b8a
sanderdona <spdona@gmail.com>
2025-07-15 16:47:24 +0200

    build(deps): junit should use the cucumber engine

--- src/test/java/nl/lunarcloud/domotics/chargerservice/cucumber/CucumberRunnerTest.java
+++ src/test/java/nl/lunarcloud/domotics/chargerservice/cucumber/CucumberRunnerTest.java
@@ -1,12 +1,14 @@
 package nl.lunarcloud.domotics.chargerservice.cucumber;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
+@IncludeEngines("cucumber")
 @SelectPackages("features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber/report.html")
 public class CucumberRunnerTest {
